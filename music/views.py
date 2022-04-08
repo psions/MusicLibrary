1b55787e8f08c720dbe86b40ad2acd17c1c121dd
psions <psionreality@gmail.com>
2022-04-07 21:11:42 -0500

    Registered music model with admin site + completed post functionality for Music + updated rest framework decimal settings

--- music/views.py
+++ music/views.py
@@ -1,14 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Music
 from.serializers import MusicSerializer
-from music import serializers
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def musics_list(request):
-    musics = Music.objects.all()
-
-    serializer = MusicSerializer(musics, many=True)
-
     
-    return Response(serializer.data)
+    if request.method == 'GET':
+        musics = Music.objects.all()
+        serializer = MusicSerializer(musics, many=True)
+        return Response(serializer.data)
+
+
+    elif request.method == 'POST':
+        serializer = MusicSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
