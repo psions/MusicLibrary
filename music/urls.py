2c839eca2e43b4838e71e67856f23f7d6229811c
psions <psionreality@gmail.com>
2022-04-07 20:11:21 -0500

    Created GET for all music

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.music_list)
+    path('', views.musics_list)
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Music
+from.serializers import MusicSerializer
+from music import serializers
 
 @api_view(['GET'])
 def musics_list(request):
+    musics = Music.objects.all()
 
+    serializer = MusicSerializer(musics, many=True)
 
-    return Response('okay')
+    
+    return Response(serializer.data)
