6ae9af73d1786e7c36aa06fda6806067cc39b902
psions <psionreality@gmail.com>
2022-04-07 22:02:59 -0500

    Created DELETE function in views file

--- music/views.py
+++ music/views.py
@@ -19,19 +19,20 @@ def musics_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])   
+@api_view(['GET', 'PUT','DELETE'])   
 def music_detail(request, pk):
     music = get_object_or_404(Music, pk=pk)
     if request.method == 'GET':
         serializer = MusicSerializer(music);
         return Response(serializer.data)
-
     elif request.method == 'PUT':
         serializer= MusicSerializer(music, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-
+    elif request.method == 'DELETE':
+        music.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     
     
